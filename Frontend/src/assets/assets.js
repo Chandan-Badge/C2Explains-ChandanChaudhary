2581f22471df11a52d35abe28784b8c4b8525dd9
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-01 13:41:50 +0530

    Improvement 01

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,14 @@
+export const products = [
+    {
+        _id: "aaaaa",
+        name: "Beautiful Button",
+        description: "A beautiful button.",
+        price: 100,
+        image: [p_img1],
+        category: "button",
+        subCategory: "frontend",
+        date: 1716634345448,
+        bestseller: true
+    },
+
+]
