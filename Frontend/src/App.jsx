5dd72506603196fbe73d83beeb9a3f35cc995b17
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-01 13:58:50 +0530

    Creating Pages on different routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import {Router, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Card from './pages/Card';
+import Login from './pages/Login';
+import Payment from './pages/Payment';
+import Orders from './pages/Orders';
+import Product from './pages/Product';
+import Contact from './pages/Contact';
+import Collection from './pages/Collection';
+
 function App() {
   return (
     <div>
-      <h1 className='bg-blue-400 text-[#fff] w-max'>Hello, from UiStack (Chandan Chaudhary)</h1>
+
+        <Router>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/card' element={<Card/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/payment' element={<Payment/>} />
+          <Route path='/orders' element={<Orders/>} />
+          <Route path='/product/:productId' element={<Product/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/collection' element={<Collection/>} />
+        </Router>
+
     </div>
   )
 }
