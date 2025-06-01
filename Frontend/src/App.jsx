ad5063c58695f96e76db0df6e1b6a5a9b1793961
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-01 13:05:42 +0530

    Initialize Project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-blue-400 text-[#fff] w-max'>Hello, from UiStack (Chandan Chaudhary)</h1>
+    </div>
+  )
+}
+
+export default App;
