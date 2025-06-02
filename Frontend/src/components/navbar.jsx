fc5e675a9a239df956e0a8a5fe35804d117624d6
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-02 07:03:56 +0530

    Improvement 03

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -3,9 +3,23 @@ import {assets} from "../assets/assets.js";
 
 function Navbar() {
   return (
-    <div className='bg-pink-500 w-full h-screen'>
+    <div className='px-10 py-4 text-[#fff] flex justify-between'>
       
-      <img src={assets.logo} alt="" className='w-20 h-20' />
+      <img src={assets.logo} alt="" className='w-16 h-16' />
+
+      <div className='flex gap-5'>
+        <button>Home</button>
+        <button>Collection</button>
+        <button>About</button>
+        <button>Contact</button>
+        <button>Admin Panel</button>
+      </div>
+
+      <div className='flex gap-5'>
+        <button>Search</button>
+        <button>Cart</button>
+        <button>Profile</button>
+      </div>
     </div>
   )
 }
