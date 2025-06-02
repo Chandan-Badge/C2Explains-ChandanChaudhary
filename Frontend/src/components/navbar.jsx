6bc5380f359ff753fcaefc906953b715a131519d
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-02 18:59:32 +0530

    Navbar Completed

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -45,7 +45,7 @@ function Navbar() {
           {/* <svg xmlns="http://www.w3.org/2000/svg" className='w-8 h-8' viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg> */}
           <svg xmlns="http://www.w3.org/2000/svg" className='w-8 h-8' viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z"/></svg>
 
-          <p className='absolute -right-1 -bottom-1 w-4 text-center landing-4 text-black bg-white aspect-square rounded-full text-[8px] flex justify-center items-center'>10</p>
+          <p className='absolute -right-[2px] -bottom-[2px] w-4 text-center landing-4 text-black bg-white aspect-square rounded-full text-[8px] flex justify-center items-center'>10</p>
         </Link>
 
         <button className='group relative'>
@@ -73,11 +73,27 @@ function Navbar() {
       </div>
 
       {/* Menu option for small screen */}
-      <div onClick={() => setVisible(false)} className={`absolute top-0 bottom-0 right-0 overflow-hidden px-2 py-4 bg-[#0d1224] transition-all ${visible ? "w-full" : "w-0"}`}>
-        <div  className='w-full px-4 py-1 flex items-center cursor-pointer'>
-          <svg xmlns="http://www.w3.org/2000/svg" className='w-8 h-8' viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z"/></svg>
-          <p>Back</p>
+      <div className={`absolute top-0 bottom-0 right-0 flex flex-col overflow-hidden py-2 bg-[#0d1224] transition-all ${visible ? "w-full" : "w-0"}`}>
+        <div onClick={() => setVisible(false)} className='w-full px-4 py-3 flex items-center cursor-pointer border-b border-gray-400'>
+          <svg xmlns="http://www.w3.org/2000/svg" className='w-5 h-5' viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z"/></svg>
+          <p className='-left-10'>Back</p>
         </div>
+
+        <NavLink to="/" onClick={() => setVisible(false)} className='sidebar-menu w-full px-6 py-3 flex items-center cursor-pointer border-b border-gray-400 hover:bg-slate-200 hover:text-[#111]'>
+          <p className=''>Home</p>
+        </NavLink>
+
+        <NavLink to="/collection" onClick={() => setVisible(false)} className='sidebar-menu w-full px-6 py-3 flex items-center cursor-pointer border-b border-gray-400 hover:bg-slate-200 hover:text-[#111]'>
+          <p>Collection</p>
+        </NavLink>
+
+        <NavLink to="/About" onClick={() => setVisible(false)} className='sidebar-menu w-full px-6 py-3 flex items-center cursor-pointer border-b border-gray-400 hover:bg-slate-200 hover:text-[#111]'>
+          <p>About</p>
+        </NavLink>
+
+        <NavLink to="/Contact" onClick={() => setVisible(false)} className='sidebar-menu w-full px-6 py-3 flex items-center cursor-pointer border-b border-gray-400 hover:bg-slate-200 hover:text-[#111]'>
+          <p>Contact</p>
+        </NavLink>
       </div>
       
     </div>
