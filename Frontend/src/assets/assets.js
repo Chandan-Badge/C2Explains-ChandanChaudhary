bd8caf96de6b33affbe5d07386a6cb0ee14df177
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-07 19:46:58 +0530

    Working on Products

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -6,17 +6,28 @@ import p_img3 from "./products/p_img3.png";
 import p_img4 from "./products/p_img4.png";
 import p_img5 from "./products/p_img5.png";
 import p_img6 from "./products/p_img6.png";
+import p_img7 from "./products/p_img7.png";
+import p_img8 from "./products/p_img8.png";
+import p_img9 from "./products/p_img9.png";
 
 export const assets =  {
     logo,
-    p_img6
+    p_img1,
+    p_img2,
+    p_img3,
+    p_img4,
+    p_img5,
+    p_img6,
+    p_img7,
+    p_img8,
+    p_img9,
 }
 
 export const products = [
     {
         _id: "aaaaa1",
-        name: "Animated border color",
-        description: "A beautiful button.",
+        name: "Animated border",
+        description: "A beautiful border.",
         price: 0,
         image: [p_img1],
         category: "animation",
@@ -28,7 +39,7 @@ export const products = [
     {
         _id: "aaaa2",
         name: "Autotyper",
-        description: "A beautiful button.",
+        description: "A beautiful typer.",
         price: 25,
         image: [p_img2],
         category: "js",
@@ -46,7 +57,7 @@ export const products = [
         category: "button",
         subCategory: "frontend",
         date: 1716634345448,
-        bestseller: true
+        bestseller: false
     },
 
     {
@@ -56,6 +67,66 @@ export const products = [
         price: 0,
         image: [p_img4],
         category: "button",
+        subCategory: "frontend",
+        date: 1716634345448,
+        bestseller: false
+    },
+
+    {
+        _id: "aaaa5",
+        name: "Colorful Button",
+        description: "A beautiful button.",
+        price: 0,
+        image: [p_img5],
+        category: "button",
+        subCategory: "frontend",
+        date: 1716634345448,
+        bestseller: false
+    },
+
+    {
+        _id: "aaaa6",
+        name: "Professional Button",
+        description: "A beautiful button.",
+        price: 0,
+        image: [p_img6],
+        category: "button",
+        subCategory: "frontend",
+        date: 1716634345448,
+        bestseller: false
+    },
+
+    {
+        _id: "aaaa7",
+        name: "Cool Card",
+        description: "A beautiful card.",
+        price: 0,
+        image: [p_img7],
+        category: "Card",
+        subCategory: "frontend",
+        date: 1716634345448,
+        bestseller: false
+    },
+
+    {
+        _id: "aaaa8",
+        name: "Colorfull card",
+        description: "A beautiful card.",
+        price: 0,
+        image: [p_img8],
+        category: "Card",
+        subCategory: "frontend",
+        date: 1716634345448,
+        bestseller: true
+    },
+
+    {
+        _id: "aaaa9",
+        name: "Colorful cursor Animation",
+        description: "A beautiful Cursor.",
+        price: 0,
+        image: [p_img9],
+        category: "Cursor",
         subCategory: "frontend",
         date: 1716634345448,
         bestseller: true
