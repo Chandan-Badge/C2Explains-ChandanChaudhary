9732b119d54f87de7125bac5cf3bd8ec415ec478
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-12 17:44:54 +0530

    Add to cart

--- Frontend/src/context/shopContext.jsx
+++ Frontend/src/context/shopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets.js";
 
 export const ShopContext = createContext();
@@ -10,11 +10,36 @@ export const ShopContext = createContext();
     const [search, setSearch] = useState("");
     const [showSearch, setShowSearch] = useState(false);
     const [searchBtn, setSearchBtn] = useState(true);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId) => {
+        let cartData = structuredClone(cartItems);
+
+        if(cartData[itemId]) {
+            // if(cartData[itemId]) {
+            //     cartData[itemId] += 1;
+            // }
+            // else {
+            //     cartData[itemId] = 1;
+            // }
+            cartData[itemId] += 1;
+        } else {
+            cartData[itemId] = {};
+            cartData[itemId] = 1;
+        }
+
+        setCartItems(cartData);
+    }
+
+    useEffect(() => {
+        console.log(cartItems); 
+    }, [cartItems]);
 
     const value = {
         products, currency,
         search, setSearch, showSearch, setShowSearch,
-        searchBtn, setSearchBtn
+        searchBtn, setSearchBtn,
+        cartItems, addToCart
     }
 
     return (
