54bf26cbe911faf93f1d09564400cca29ca0eec6
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-19 10:52:35 +0530

    Improvement 21

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Hero from '../components/hero';
+import Hero from '../components/Hero';
 import LatestCollection from '../components/LatestCollection';
 import BestSeller from '../components/bestSeller';
 import NewsletterBox from '../components/NewsletterBox';
 
 const Home = () => {
   return (
-    <div className='flex items-center justify-center sm:justify-end flex-col w-full pl-0 sm:pl-56'>
+    <div className='flex items-center justify-center sm:justify-end flex-col w-full pl-0 sm:pl-32 md:pl-56'>
       <Hero />
       <LatestCollection />
       <BestSeller />
