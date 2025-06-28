6e10a1e2cdae1852fb04511c3a259ccf7ba0307a
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-28 06:26:30 +0530

    Cart page design

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -44,8 +44,16 @@ function Cart() {
 
                   <div>
                     <p className='text-lg font-medium' >{productData.name}</p>
+                    <div className='flex gap-5 mt-2 items-center'>
+                      <p>{currency}{productData.price}</p>
+                    </div>
                   </div>
 
+                </div>
+
+                <div className='flex gap-5 items-center'>
+                  <input type="number" min={1} defaultValue={item.quantity} className='border-10 max-w-20 px-1 sm:px-2 py-1 bg-slate-600' />
+                  <svg xmlns="http://www.w3.org/2000/svg" className={`w-5 sm:w-7 h-5 sm:h-7 fill-[#e3e3e3] cursor-pointer`} viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
                 </div>
               </div>
             )
