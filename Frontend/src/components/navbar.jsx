a87f73471e093533630a8f90babfdb0a4dbfa3b8
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-07-04 21:44:26 +0530

    Improvement 31

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -165,7 +165,9 @@ function Navbar() {
         </Link>
 
         <button onClick={() => setProfileBtn(true)} className='group relative p-1.5 sm:p-2 rounded-full hover:bg-[#181e33] cursor-pointer transition-all duration-300 hover:shadow-[0_0_10px_rgba(38,255,226,0.1)] active:scale-95'>
-          <svg xmlns="http://www.w3.org/2000/svg" className='w-5 sm:w-7 h-5 sm:h-7 fill-[#e3e3e3]' viewBox="0 -960 960 960"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
+          <Link to={"/login"}>
+            <svg xmlns="http://www.w3.org/2000/svg" className='w-5 sm:w-7 h-5 sm:h-7 fill-[#e3e3e3]' viewBox="0 -960 960 960"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
+          </Link>
 
           {/* <div class="absolute invisible group-hover:visible opacity-0 group-hover:opacity-100 transition-all duration-300 mb-2 left-1/2 transform -translate-x-1/2 z-50 -bottom-10 md:-bottom-12">
               <div class="bg-black/70 backdrop-blur-md border-[#181e33] text-[#26ffe3] text-xs sm:text-sm px-1.5 sm:px-2.5 py-1 sm:py-1.5 rounded-lg whitespace-nowrap font-light tracking-wider shadow-[0_0_15px_rgba(0,0,0,0.3)] border">
--- Frontend/src/pages/Orders.jsx
+++ Frontend/src/pages/Orders.jsx
@@ -7,7 +7,7 @@ function Orders() {
   const {products, currency} = useContext(ShopContext);
 
   return (
-    <div className='min-h-[80vh] mt-20 md:mt-24 ml-0 sm:ml-36 md:ml-56 px-4 sm:px-8'>
+    <div className='min-h-[80vh] mt-24 md:mt-24 ml-0 sm:ml-40 md:ml-48 px-6 sm:px-8'>
       
       <div className='text-2xl'>
         <Title text1={"MY"} text2={"ORDERS"} />
@@ -17,7 +17,7 @@ function Orders() {
         {
           products.slice(1, 4).map((item, index) => (
             <div key={index} className='py-4 border-b text-gray-300 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
-              <div className='flex items-start gap-4 text-sm'>
+              <div className='flex items-center gap-4 text-sm'>
                 <img src={item.image[0]} className='w-16 sm:w-20' alt="" />
                 <div>
                   <p className='text-base sm:text-lg font-medium'>{item.name}</p>
@@ -30,9 +30,12 @@ function Orders() {
                 </div>
               </div>
 
-              <div>
-                <button className='text-sm bg-gray-800 hover:bg-gray-900 text-white px-8 py-3 border border-gray-500 transition-all duration-200 ease-in-out'>View Details</button>
+              <div className='flex items-center gap-2'>
+                <span className='min-w-2 h-2 rounded-full bg-green-500'></span>
+                <p className='text-sm md:text-base'>Success</p>
               </div>
+
+              <button className='w-max text-sm bg-gray-800 hover:bg-gray-900 text-white px-8 py-3 border border-gray-500 transition-all duration-200 ease-in-out'>View Details</button>
             </div>
           ))
         }
