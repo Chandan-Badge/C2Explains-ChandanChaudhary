747ed16efab0fd0f3384abd146a257a11e01b253
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-07-04 18:03:26 +0530

    Improvement 31

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -9,6 +9,7 @@ function Navbar() {
 
     const [visible, setVisible] = useState(false);
     const {showSearch, setShowSearch, searchBtn, setSearchBtn, getCartCount} = useContext(ShopContext);
+    const {navigate} = useContext(ShopContext);
 
     // const [searchBtn, setSearchBtn] = useState(true);
 
@@ -178,7 +179,7 @@ function Navbar() {
           <div className={`group-hover:block hidden {profile} dropdown-menu -right-5 pt-4 absolute`}>
             <div className='bg-[#0d1224] text-gray-300 rounded border border-[#b6bee5]/50 flex flex-col gap-2 w-36 px-3 py-4'>
               <p className='cursor-pointer hover:text-[#fff] hover:bg-gray-700/50 px-4 py-1 rounded transition-all ease-in-out duration-200'>My Profile</p>
-              <p className='cursor-pointer hover:text-[#fff] hover:bg-gray-700/50 px-4 py-1 rounded transition-all ease-in-out duration-200'>Orders</p>
+              <p onClick={() => navigate("/orders")} className='cursor-pointer hover:text-[#fff] hover:bg-gray-700/50 px-4 py-1 rounded transition-all ease-in-out duration-200'>Orders</p>
               <p className='cursor-pointer text-red-500 hover:text-[#fff] hover:bg-red-700 px-4 py-1 rounded transition-all ease-in-out duration-200'>Logout</p>
             </div>
           </div>
--- Frontend/src/pages/Orders.jsx
+++ Frontend/src/pages/Orders.jsx
@@ -16,11 +16,23 @@ function Orders() {
       <div>
         {
           products.slice(1, 4).map((item, index) => (
-            <div key={index} className='py-4 border-t text-gray-300 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
+            <div key={index} className='py-4 border-b text-gray-300 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
               <div className='flex items-start gap-4 text-sm'>
                 <img src={item.image[0]} className='w-16 sm:w-20' alt="" />
+                <div>
+                  <p className='text-base sm:text-lg font-medium'>{item.name}</p>
+                  <div className='flex items-center gap-3 mt-2 text-base text-gray-300'>
+                    <p className='text-base'>{currency}{item.price}</p>
+                    <p>Quantity: 1</p>
+                  </div>
+
+                  <p className='mt-2'>Data: <span className='text-gray-400'>04/06/2025</span></p>
+                </div>
               </div>
 
+              <div>
+                <button className='text-sm bg-gray-800 hover:bg-gray-900 text-white px-8 py-3 border border-gray-500 transition-all duration-200 ease-in-out'>View Details</button>
+              </div>
             </div>
           ))
         }
