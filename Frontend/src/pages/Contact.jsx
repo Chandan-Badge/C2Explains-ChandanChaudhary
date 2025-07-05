d2c7ce63661d757a3d78b6da83a62d04710c3123
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-07-05 17:32:28 +0530

    Contact us page Improvement

--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Title from "../components/Title.jsx";
-import Coder from "../assets/coder.png";
+import Coder from "../assets/coder2.png";
 
 function Contact() {
   return (
@@ -9,39 +9,38 @@ function Contact() {
         <Title text1={"CONTACT"} text2={"US"} />
       </div>
 
-      <div class="flex flex-col lg:flex-row items-center justify-between mt-10 mx-auto px-8 gap-12">
-      {/* <h2 class="not-show-pop text-2xl sm:text-4xl text-center relative z-10 font-bold px-4 py-2 w-max mx-auto mt-14 text-[#0f6dbf] dark:bg-[#0b0d14] bg-[#fff] border-2 rounded border-[#0f6dbf]">Let's Talk?</h2> */}
+      <div className="flex flex-col lg:flex-row items-center justify-between mt-10 mx-auto px-8 gap-12">
+      {/* <h2 className="not-show-pop text-2xl sm:text-4xl text-center relative z-10 font-bold px-4 py-2 w-max mx-auto mt-14 text-[#b6bee5] dark:bg-[#0b0d14] bg-[#fff] border-2 rounded border-[#b6bee5]">Let's Talk?</h2> */}
 
-        <div class="w-[60%] lg:max-w-[30vw] h-max">
-          <img src={Coder} alt="connect" class="w-max" />
+        <div className="w-[60%] lg:max-w-[30vw] h-max">
+          <img src={Coder} alt="connect" className="w-max" />
         </div>
 
         <div class="not-show-pop-slow w-full max-w-[600px] flex justify-center">
-          <form class="w-[80%] sm:w-full" name="contactUS" netlify>
-            
-            {/* <!-- <form class="max-w-md mx-auto"> --> */}
-              <p class="dark:text-[#0f6dbf] text-[#0f6dbf] mb-5 dark:hover:text-[#fff] transition-all duration-300 ease-in-out">New projects, freelance inquiry or even a coffee.</p>
+          <form className="w-[80%] sm:w-full" name="contactUS" netlify>
 
-              <div class="relative z-0 w-full mb-5 group">
-                  <input type="text" name="name" id="name" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white dark:border-gray-600 focus:border-[#0f6dbf] text-gray-900 dark:focus:border-[#0f6dbf] border-gray-300 focus:outline-none focus:ring-0 peer" placeholder=" " required />
-                  <label for="name" class="peer-focus:font-medium absolute text-sm dark:text-gray-400 text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto  peer-focus:text-[#0f6dbf] dark:peer-focus:text-[#0f6dbf] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your name *</label>
+              <p className="dark:text-[#b6bee5] text-[#b6bee5] mb-5 dark:hover:text-[#fff] transition-all duration-300 ease-in-out">New projects, freelance inquiry or even a coffee.</p>
+
+              <div className="relative z-0 w-full mb-5 group">
+                  <input type="text" name="name" id="name" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white dark:border-gray-600 focus:border-[#b6bee5] text-gray-900 dark:focus:border-[#b6bee5] border-gray-300 focus:outline-none focus:ring-0 peer" placeholder=" " required />
+                  <label for="name" className="peer-focus:font-medium absolute text-sm dark:text-gray-400 text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto  peer-focus:text-[#b6bee5] dark:peer-focus:text-[#b6bee5] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your name *</label>
               </div>
-              <div class="relative z-0 w-full mb-5 group">
-                  <input type="email" name="email" id="email" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white dark:border-gray-600 focus:border-[#0f6dbf] text-gray-900 dark:focus:border-[#0f6dbf] border-gray-300 focus:outline-none focus:ring-0 peer" placeholder=" " required />
-                  <label for="email" class="peer-focus:font-medium absolute text-sm dark:text-gray-400 text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto  peer-focus:text-[#0f6dbf] dark:peer-focus:text-[#0f6dbf] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address *</label>
+              <div className="relative z-0 w-full mb-5 group">
+                  <input type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white dark:border-gray-600 focus:border-[#b6bee5] text-gray-900 dark:focus:border-[#b6bee5] border-gray-300 focus:outline-none focus:ring-0 peer" placeholder=" " required />
+                  <label for="email" className="peer-focus:font-medium absolute text-sm dark:text-gray-400 text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto  peer-focus:text-[#b6bee5] dark:peer-focus:text-[#b6bee5] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address *</label>
               </div>
-              <div class="relative z-0 w-full mb-5 group">
-                  <input type="text" name="subject" id="subject" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white dark:border-gray-600 focus:border-[#0f6dbf] text-gray-900 dark:focus:border-[#0f6dbf] border-gray-300 focus:outline-none focus:ring-0 peer" placeholder=" " required />
-                  <label for="subject" class="peer-focus:font-medium absolute text-sm dark:text-gray-400 text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto  peer-focus:text-[#0f6dbf] dark:peer-focus:text-[#0f6dbf] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Subject *</label>
+              <div className="relative z-0 w-full mb-5 group">
+                  <input type="text" name="subject" id="subject" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white dark:border-gray-600 focus:border-[#b6bee5] text-gray-900 dark:focus:border-[#b6bee5] border-gray-300 focus:outline-none focus:ring-0 peer" placeholder=" " required />
+                  <label for="subject" className="peer-focus:font-medium absolute text-sm dark:text-gray-400 text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto  peer-focus:text-[#b6bee5] dark:peer-focus:text-[#b6bee5] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Subject *</label>
               </div>
 
-              <div class="relative z-0 w-full mb-5 group">
-                <textarea name="massage" id="massage" rows="8" class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white dark:border-gray-600 focus:border-[#0f6dbf] text-gray-900 dark:focus:border-[#0f6dbf] border-gray-300 focus:outline-none focus:ring-0 peer"></textarea>
-                <label for="textarea" class="ppeer-focus:font-medium absolute text-sm dark:text-gray-400 text-gray-500 duration-300 transform -translate-y-6 scale-100 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto  peer-focus:text-[#0f6dbf] dark:peer-focus:text-[#0f6dbf] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-100 peer-focus:-translate-y-6">Massage *</label>
+              <div className="relative z-0 w-full mb-5 group">
+                <textarea name="massage" id="massage" rows="8" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white dark:border-gray-600 focus:border-[#b6bee5] text-gray-900 dark:focus:border-[#b6bee5] border-gray-300 focus:outline-none focus:ring-0 peer"></textarea>
+                <label for="textarea" className="ppeer-focus:font-medium absolute text-sm dark:text-gray-400 text-gray-500 duration-300 transform -translate-y-6 scale-100 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto  peer-focus:text-[#b6bee5] dark:peer-focus:text-[#b6bee5] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-100 peer-focus:-translate-y-6">Massage *</label>
             </div>
 
             {/* <!-- Button --> */}
-            <button type="submit" class="w-full text-white bg-[#0f6dbf] hover:bg-[#0f6dbfd8] focus:ring-2 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 me-2 mb-2">Send Massage</button>
+            <button type="submit" className="w-full bg-gray-800 hover:bg-gray-900 text-white border border-gray-700 focus:ring-2 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 me-2 mb-2">Send Massage</button>
           </form>
 
         </div>
