a6872f0fab672af10c1e9f91696e00bda96bd431
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-07-11 19:59:44 +0530

    Admin Panel Started

---
+++ Admin/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      Admin Panel
+    </>
+  )
+}
+
+export default App;
--- Backend/middlewares/adminAuth.js
+++ Backend/middlewares/adminAuth.js
@@ -3,16 +3,16 @@ import jwt from "jsonwebtoken";
 const adminAuth = async (req, res, next) => {
 
     try {
-        const {token} = req.header;
+        const {token} = req.headers;
     
         if(!token) {
-            return res.json({success: false, message: "Not autheorized Please login."})
+            return res.json({success: false, message: "Not autheorized Please login."});
         }
     
         const token_decode = jwt.verify(token, process.env.JWT_SECRET);
     
         if(token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
-            return res.json({success: false, message: "Not autheorized Please login."})
+            return res.json({success: false, message: "Not autheorized, details not matched."});
         }
     
         next();
