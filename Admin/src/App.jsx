873dcc512289e48c48f6fd092a4e0d160ba2ac8e
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-07-13 15:44:05 +0530

    Admin Frontend 05

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -5,21 +5,30 @@ import Add from './Pages/Add';
 import List from './Pages/List';
 import Orders from './Pages/Orders';
 import Navbar from './Components/Navbar';
+import Sidebar from './Components/Sidebar';
 
 function App() {
 
   return (
-    <>
-      <div className='text-white'>
+    <div className='text-white min-h-screen'>
+      <>
         <Navbar />
-        {/* <Routes>
-          <Route path='/' element={<Add />} />
-          <Route path='/add' element={<Add />} />
-          <Route path='/list' element={<List />} />
-          <Route path='/orders' element={<Orders />} />
-        </Routes> */}
-      </div>
-    </>
+
+        <div className='flex w-full'>
+          <Sidebar />
+
+          <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-base'>
+            <Routes>
+              {/* <Route path='/' element={<Add />} /> */}
+              <Route path='/add' element={<Add />} />
+              <Route path='/list' element={<List />} />
+              <Route path='/orders' element={<Orders />} />
+            </Routes>
+          </div>
+        </div>
+
+      </>
+    </div>
   )
 }
 
--- Admin/src/Components/Sidebar.jsx
+++ Admin/src/Components/Sidebar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Sidebar() {
   return (
-    <div>
-      Sidebar
+    <div className='w-[18%] min-h-screen border-r-2 border-r-gray-600'>
+      <div className='admin-sidebar flex flex-col gap-4 pt-6 pl-[20%] text-base'>
+
+        <NavLink to={"/add"} className="flex items-center gap-3 border border-gray-500 border-r-0 px-3 py-2 rounded-l">
+          <p>Add Items</p>
+        </NavLink>
+
+        <NavLink to={"/list"} className="flex items-center gap-3 border border-gray-500 border-r-0 px-3 py-2 rounded-l">
+          <p>List Items</p>
+        </NavLink>
+
+        <NavLink to={"/orders"} className="flex items-center gap-3 border border-gray-500 border-r-0 px-3 py-2 rounded-l">
+          <p>Orders</p>
+        </NavLink>
+      </div>
+
     </div>
   )
 }
