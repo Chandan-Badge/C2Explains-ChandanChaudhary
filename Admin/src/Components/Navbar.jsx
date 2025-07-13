4dc7761d654f3757fd0aba3e1ab2262004d38f3c
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-07-13 09:19:32 +0530

    Improvement 36

--- Admin/src/Components/Navbar.jsx
+++ Admin/src/Components/Navbar.jsx
@@ -4,8 +4,8 @@ import { assets } from "../assets/assets.js";
 function Navbar() {
   return (
     <div className='px-4 sm:px-6 md:px-10 py-5 h-[10vh] flex justify-between items-center border-b border-b-[#b6bee5]/50 top-0 w-full z-10'>
-      <img src={assets.newLogo} className='w-32 md:w-44' alt="" />
-      <button className='font-medium text-sm sm:text-base bg-gray-800 hover:bg-gray-900 border border-gray-700 py-1.5 px-5 transition-all duration-300 ease-in-out'>Log out</button>
+      <img src={assets.colorLogo} className='w-32 md:w-44' alt="" />
+      <button className='font-medium text-sm sm:text-base bg-gray-800 hover:bg-gray-900 border border-gray-700 py-2 px-5 transition-all duration-300 ease-in-out'>Log out</button>
     </div>
   )
 }
--- Admin/src/assets/assets.js
+++ Admin/src/assets/assets.js
@@ -1,7 +1,9 @@
 import logo from "./C2-Logo.png";
 import newLogo from "./C2-Logo-New.png";
+import colorLogo from "./C2-Logo-New Color.png";
 
 export const assets = {
     logo,
-    newLogo
+    newLogo,
+    colorLogo
 }
