71953d7f164593f47a0f04e1b11e5dc3882d8e7f
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-07-15 20:55:48 +0530

    Improvement 37

--- Admin/src/Components/Login.jsx
+++ Admin/src/Components/Login.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = async (e) => {
+    try {
+      e.preventDefault();
+
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <div className='min-h-screen flex items-center justify-center w-full'>
       <div className='border border-white/60 shadow-md rounded-md px-8 py-6 max-w-md'>
         <h1 className='text-2xl font-bold mb-4 text-[#b6bee5]'>Admin <span className='text-gray-200'>Panel</span></h1>
 
-        <form action="" className='flex flex-col gap-4'>
+        <form action="" onSubmit={submitHandler} className='flex flex-col gap-4'>
           <div className='mb-3 min-w-72 flex flex-col gap-1'>
             <p className='text-sm font-medium text-gray-300'>Email Address: </p>
-            <input className='rounded w-full px-3 py-2 bg-gray-900 border border-gray-500 outline-none' type="email" />
+            <input onChange={(e) => setEmail(e.target.value)} value={email} className='rounded w-full px-3 py-2 bg-gray-900 border border-gray-500 outline-none' type="email" placeholder='Email' required />
           </div>
 
           <div className='mb-3 min-w-72 flex flex-col gap-1'>
             <p className='text-sm font-medium text-gray-300'>Password: </p>
-            <input className='rounded w-full px-3 py-2 bg-gray-900 border border-gray-500 outline-none' type="email" />
+            <input onChange={(e) => setPassword(e.target.value)} value={password} className='rounded w-full px-3 py-2 bg-gray-900 border border-gray-500 outline-none' type="password" placeholder='Password' required />
           </div>
 
           <button className='pt-2 mb-2 w-full py-2 px-4 font-medium bg-gray-800 hover:bg-gray-900 text-white border border-gray-700 transition-all duration-200 ease-in-out'>Login</button>
