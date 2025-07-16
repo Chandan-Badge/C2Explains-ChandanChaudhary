4292b1d30e1cd4d40c5ae3a3916cf88a6f28211d
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-07-16 16:31:33 +0530

    Improvement 38

--- Admin/src/Pages/Add.jsx
+++ Admin/src/Pages/Add.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { assets } from '../assets/assets.js';
 
 function Add() {
+
+  const [image1, setImage1] = useState(false);
+  const [image2, setImage2] = useState(false);
+  const [image3, setImage3] = useState(false);
+  const [image4, setImage4] = useState(false);
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = ("");
+  const [category, setCategory] = useState("Button");
+  const [subCategory, setSubCategory] = useState("Frontend");
+  const [type, setType] = useState("Free");
+  const [bestSeller, setBestSeller] = useState(false);
+
   return (
     <form className='flex flex-col w-full items-start gap-4'>
       <h1 className='text-4xl mb-2 font-bold text-[#c586a5]'>Add Product</h1>
@@ -10,23 +24,24 @@ function Add() {
 
         <div className='flex gap-5'>
           <label htmlFor="image1">
-            <svg xmlns="http://www.w3.org/2000/svg" className='w-14' viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M440-320v-326L336-542l-56-58 200-200 200 200-56 58-104-104v326h-80ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
-            <input type="file" id="image1" hidden />
+            <img src={!image1 ? assets.uploadFile : URL.createObjectURL(image1)} className='w-14' alt="" />
+            {/* <svg xmlns="http://www.w3.org/2000/svg" className='w-14' viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M440-320v-326L336-542l-56-58 200-200 200 200-56 58-104-104v326h-80ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg> */}
+            <input onChange={(e) => setImage1(e.target.files[0])} type="file" id="image1" hidden />
           </label>
 
           <label htmlFor="image2">
-            <svg xmlns="http://www.w3.org/2000/svg" className='w-14' viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M440-320v-326L336-542l-56-58 200-200 200 200-56 58-104-104v326h-80ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
-            <input type="file" id="image2" hidden />
+            <img src={!image2 ? assets.uploadFile : URL.createObjectURL(image2)} className='w-14' alt="" />
+            <input onChange={(e) => setImage2(e.target.files[0])} type="file" id="image2" hidden />
           </label>
 
           <label htmlFor="image3">
-            <svg xmlns="http://www.w3.org/2000/svg" className='w-14' viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M440-320v-326L336-542l-56-58 200-200 200 200-56 58-104-104v326h-80ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
-            <input type="file" id="image3" hidden />
+            <img src={!image3 ? assets.uploadFile : URL.createObjectURL(image3)} className='w-14' alt="" />
+            <input onChange={(e) => setImage3(e.target.files[0])} type="file" id="image3" hidden />
           </label>
 
           <label htmlFor="image4">
-            <svg xmlns="http://www.w3.org/2000/svg" className='w-14' viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M440-320v-326L336-542l-56-58 200-200 200 200-56 58-104-104v326h-80ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
-            <input type="file" id="image4" hidden />
+            <img src={!image4 ? assets.uploadFile : URL.createObjectURL(image4)} className='w-14' alt="" />
+            <input onChange={(e) => setImage4(e.target.files[0])} type="file" id="image4" hidden />
           </label>
         </div>
       </div>
--- Admin/src/assets/assets.js
+++ Admin/src/assets/assets.js
@@ -1,11 +1,13 @@
 import logo from "./C2-Logo.png";
 import newLogo from "./C2-Logo-New.png";
 import colorLogo from "./C2-Logo-New Color.png";
-import uploadArea from "./upload_area.png"
+import uploadArea from "./upload_area.png";
+import uploadFile from "./upload_file.svg";
 
 export const assets = {
     logo,
     newLogo,
     colorLogo,
-    uploadArea
+    uploadArea,
+    uploadFile
 }
