88096837921f53394572ad1157500268c46e007f
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-07-18 07:55:21 +0530

    fixing bugs and making list page

--- Admin/src/Pages/List.jsx
+++ Admin/src/Pages/List.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { backendUrl } from '../App';
+import { toast } from 'react-toastify';
 
 function List() {
 
@@ -12,9 +13,16 @@ function List() {
       const response = await axios.get(backendUrl + '/api/product/list');
       console.log(response.data);
 
+      if(response.data.success) {
+        setList(response.data.products);
+      }
+      else {
+        toast.error(response.data.message);
+      }
+
     } catch (error) {
       console.log(error);
-      console.log(error.message);
+      toast.error(error.message);
     }
   }
 
@@ -24,7 +32,18 @@ function List() {
 
   return (
     <div>
-      <p>List page</p>
+      <p className='mb-2 font-bold text-2xl'>All Product List</p>
+      <div className='flex flex-col gap-2'>
+
+        <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] text-sm font-semibold bg-gray-800/50 items-center py-2 px-4 border border-gray-400'>
+          <p>Image</p>
+          <p>Name</p>
+          <p>Category</p>
+          <p>Price</p>
+          <p>Action</p>
+        </div>
+
+      </div>
     </div>
   )
 }
