04723c197a5ef53365a887345966f6e5d015960a
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-07-26 17:54:42 +0530

    Improvement 41

--- Backend/controllers/cart.controller.js
+++ Backend/controllers/cart.controller.js
@@ -4,21 +4,21 @@ import userModel from "../models/user.model.js";
 // add products to user cart
 const addToCart = async (req, res) => {
     try {
-        const { userId, itemId, size } = req.body;
+        const { userId, itemId } = req.body;
 
         const userData = await userModel.findById(userId);
         let cartData = await userData.cartData;
 
         if(cartData[itemId]) {
-            if(cartData[itemId][size]) {
-                cartData[itemId][size] += 1;
-            }
-            else {
-                cartData[itemId][size] = 1;
-            }
+            // if(cartData[itemId]) {
+                cartData[itemId] += 1;
+            // }
+            // else {
+            //     cartData[itemId] = 1;
+            // }
         } else {
             cartData[itemId] = {};
-            cartData[itemId][size] = 1;
+            cartData[itemId] = 1;
         }
 
         await userModel.findByIdAndUpdate(userId, {cartData});
@@ -35,12 +35,12 @@ const addToCart = async (req, res) => {
 const updateCart = async (req, res) => {
     
     try {
-        const { userId, itemId, size, quantity } = req.body;
+        const { userId, itemId, quantity } = req.body;
 
         const userData = await userModel.findById(userId);
         let cartData = await userData.cartData;
 
-        cartData[itemId][size] = quantity;
+        cartData[itemId] = quantity;
 
         await userModel.findByIdAndUpdate(userId, {cartData});
 
