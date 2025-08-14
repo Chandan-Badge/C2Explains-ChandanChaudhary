117e3aa5d52ac6fc3ed175b1c69e7e402285cfb6
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-08-14 20:00:04 +0530

    creating order controller

--- Backend/controllers/order.controller.js
+++ Backend/controllers/order.controller.js
@@ -1,32 +1,88 @@
+import orderModel from "../models/order.model"
 
 
 // ==> Placing order using COD
 const placeOrder = async () => {
 
+    try {
+        
+    } catch (error) {
+        
+    }
+    
 }
 
 // ==> Placing order using Stripe
-const placeOrderStripe = async () => {
+const placeOrderStripe = async (req, res) => {
+
+    try {
+        const { userId, items, amount } = req.body;
     
+        const orderData = {
+            userId,
+            items,
+            amount,
+            paymentMethod: "POS",
+            payment: false,
+            date: Date.now()
+        };
+    
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();
+    
+        await userModel.findByIdAndUpdate(userId, {cartData:{}});
+    
+        res.json({success:true, message: "Item Purchased"});
+        
+    } catch (error) {
+
+        console.log(error);
+        res.json({success:false, message: error.message});
+    }
+
 }
 
 // ==> Placing order using Razorpay
 const placeOrderRazorpay = async () => {
     
+    try {
+        
+    } catch (error) {
+        
+    }
+
 }
 
 // ==> All orders data for Admin Panel
 const allOrders = async () => {
+        
+    try {
+        
+    } catch (error) {
+        
+    }
     
 }
 
 // ==> User order data for frontend
 const userOrders = async () => {
+        
+    try {
+        
+    } catch (error) {
+        
+    }
     
 }
 
 // ==> Admin update the order status
 const updateStatus = async () => {
+        
+    try {
+        
+    } catch (error) {
+        
+    }
     
 }
 
