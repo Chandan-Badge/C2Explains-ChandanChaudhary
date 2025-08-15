bccbde71215707cec672de790fd84f2097fcff58
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-08-15 08:58:04 +0530

    Improvement 48

--- Frontend/src/pages/Payment.jsx
+++ Frontend/src/pages/Payment.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 function Payment() {
 
   const [method, setMethod] = useState();
-  const {navigate} = useContext(ShopContext);
+  const {navigate, backendUrl, token, cartItem, setCartItems, getCartAmount, products } = useContext(ShopContext);
 
   const [formData, setFormData] = useState({
     firstName:"",
@@ -22,8 +22,30 @@ function Payment() {
     setFormData(data => ({...data, [name]:value}))
   }
 
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      
+      let orderItems = [];
+
+      for(const items in cartItem) {
+        for(const item in cartItem[items]) {
+          if(cartItem[items][item] > 0) {
+            const itemInfo = structuredClone(products.find(product => product._id === items));
+
+            itemInfo.quantity = cartItem[items][item]
+          }
+        }
+      }
+
+    } catch (error) {
+      
+    }
+  }
+
   return (
-    <form className='flex gap-4 pt-5 sm:pt-14 min-h-[80vh] mt-6 md:mt-10 ml-0 sm:ml-36 md:ml-56 px-4 sm:px-8'>
+    <form onSubmit={onSubmitHandler} className='flex gap-4 pt-5 sm:pt-14 min-h-[80vh] mt-6 md:mt-10 ml-0 sm:ml-36 md:ml-56 px-4 sm:px-8'>
       <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
 
         <div className='text-2xl sm:text-3xl my-3'>
@@ -31,18 +53,18 @@ function Payment() {
         </div>
 
         <div className='flex gap-3'>
-          <input onChange={onChangeHandler} name="firstName" value={formData.firstName} type="text" className='border border-gray-300 bg-gray-900 rounded py-1.5 px-3.5 w-full' placeholder='First name' />
-          <input onChange={onChangeHandler} name="lastName" value={formData.lastName} type="text" className='border border-gray-300 bg-gray-900 rounded py-1.5 px-3.5 w-full' placeholder='Last name' />
+          <input onChange={onChangeHandler} name="firstName" value={formData.firstName} type="text" className='border border-gray-300 bg-gray-900 rounded py-1.5 px-3.5 w-full' placeholder='First name' required />
+          <input onChange={onChangeHandler} name="lastName" value={formData.lastName} type="text" className='border border-gray-300 bg-gray-900 rounded py-1.5 px-3.5 w-full' placeholder='Last name' required />
         </div>
         
-        <input type="email" className='border border-gray-300 bg-gray-900 rounded py-1.5 px-3.5 w-full' placeholder='enter email' />
+        <input onChange={onChangeHandler} name="email" value={formData.email} type="email" className='border border-gray-300 bg-gray-900 rounded py-1.5 px-3.5 w-full' placeholder='enter email' required />
 
         <div className='mt-4 min-w-80 text-white text-2xl sm:text-3xl'>
           <CartTotal />
         </div>
 
         <div className='w-full mt-4 text-center'>
-          <button onClick={() => navigate("/orders")} className='font-medium text-sm bg-gray-800 hover:bg-gray-900 text-white border border-gray-700 px-16 py-3 transition-all duration-200 ease-in-out'>Complete Purchase</button>
+          <button  type='submit' className='font-medium text-sm bg-gray-800 hover:bg-gray-900 text-white border border-gray-700 px-16 py-3 transition-all duration-200 ease-in-out'>Complete Purchase</button>
         </div>
       </div>
       
