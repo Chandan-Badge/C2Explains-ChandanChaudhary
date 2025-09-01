f9b2d5c04646011f2fe013e1032cf3ead1842c64
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-09-01 22:03:03 +0530

    Order page for admin

--- Admin/src/Pages/Orders.jsx
+++ Admin/src/Pages/Orders.jsx
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import { backendUrl } from "../App.jsx";
+import { toast } from 'react-toastify';
 
-function Orders() {
+function Orders({ token }) {
+
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllData = async () => {
+    if(!token) {
+      return null;
+    }
+
+    try {
+      const response = await axios.post(backendUrl + "/api/orders/list", {}, {headers: {token}});
+      // console.log(response.data);
+
+      if(response.data.success) {
+        setOrders(response.data.orders);
+      } else {
+        toast.error(response.data.message);
+      }
+
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(() => {
+    fetchAllData();
+  }, [token])
+
   return (
     <div>
       <p>Orders page</p>
