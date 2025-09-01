ddec509c1911a6040ab05e61391085309327cff4
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-09-01 22:52:52 +0530

    admin order page

--- Admin/src/Pages/Orders.jsx
+++ Admin/src/Pages/Orders.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import { backendUrl } from "../App.jsx";
 import { toast } from 'react-toastify';
+import { assets } from '../assets/assets.js';
 
 function Orders({ token }) {
 
@@ -18,12 +19,14 @@ function Orders({ token }) {
 
       if(response.data.success) {
         setOrders(response.data.orders);
+        
       } else {
         toast.error(response.data.message);
       }
 
     } catch (error) {
-      
+      console.log(error);
+      toast.error(error.message)
     }
   }
 
@@ -33,7 +36,28 @@ function Orders({ token }) {
 
   return (
     <div>
-      <p>Orders page</p>
+      {/* <p>Orders page</p> */}
+      <h3>Order Page1</h3>
+      <div>
+        {
+          orders.map((order, index) => {
+            <div key={index}>
+              {/* <img src={assets} alt="" /> */}
+              <div>
+                {order.items.map((item, index) => {
+                  if(index === order.items.length - 1) {
+
+                    return <p key={index}> {item.name} x {item.quantity} </p>
+
+                  } else {
+                    return <p key={index}> {item.name} x {item.quantity}, </p>
+                  }
+                })}
+              </div>
+            </div>
+          })
+        }
+      </div>
     </div>
   )
 }
