cca56008820628b5fbf86ca0f7c561a0e3e11a81
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-09-02 20:10:20 +0530

    Improvement 59

--- Admin/src/Pages/Orders.jsx
+++ Admin/src/Pages/Orders.jsx
@@ -37,37 +37,58 @@ function Orders({ token }) {
   return (
     <div>
       {/* <p>Orders page</p> */}
-      <h3 className='text-4xl font-bold'>Order Page</h3>
+      <h3 className='text-3xl font-bold text-[#b6bee5] mb-2'>Order Page</h3>
+
       <div>
+        <div className='grid grid-cols-[3fr_2fr_1fr_1.5fr] text-sm font-semibold bg-gray-800/80 items-center py-2 px-4 border border-gray-400/80'>
+          <p className=''>Product</p>
+          <p className=''>Product Details</p>
+          <p>Price</p>
+          <p className='text-center'>Status</p>
+        </div>
+
         {
           orders.map((order, index) => (
-            <div key={index} className='flex justify-between mt-10'>
+            <div key={index} className='grid grid-cols-[3fr_2fr_1fr_1.5fr] items-center mt-2 border border-white/30 py-2 px-4'>
               {/* <img src={assets} alt="" /> */}
 
               <div>
 
-                <div>
-                  {order.items.map((item, index) => {
-                    if (index === order.items.length - 1) {
+                <div className='flex items-center gap-2'>
+                  <svg xmlns="http://www.w3.org/2000/svg" className='w-14' viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M440-183v-274L200-596v274l240 139Zm80 0 240-139v-274L520-457v274Zm-40-343 237-137-237-137-237 137 237 137ZM160-252q-19-11-29.5-29T120-321v-318q0-22 10.5-40t29.5-29l280-161q19-11 40-11t40 11l280 161q19 11 29.5 29t10.5 40v318q0 22-10.5 40T800-252L520-91q-19 11-40 11t-40-11L160-252Zm320-228Z"/></svg>
 
-                      return <p key={index}> {item.name} x {item.quantity} </p>
+                  <div className='flex flex-col'>
+                    {order.items.map((item, index) => {
+                      if (index === order.items.length - 1) {
 
-                    } else {
-                      return <p key={index}> {item.name} x {item.quantity}, </p>
-                    }
-                  })}
+                        return <p key={index}> {item.name} x {item.quantity} </p>
+
+                      } else {
+                        return <p key={index}> {item.name} x {item.quantity}, </p>
+                      }
+                    })}
+                  </div>
                 </div>
                 {/* <p> {order.firstName + " " + order.lastName} </p> */}
               </div>
 
-              <div>
-                <p>Items: {order.items.length}</p>
-                <p>Method: {order.paymentMethod}</p>
-                <p>Payment: { order.payment ? "Done" : "Pending"}</p>
-                <p>Date: {new Date(order.date).toLocaleDateString() }</p>
+              <div className=''>
+                <p className='text-[#b6bee5]'>Items: <span className='text-[#fff]'> {order.items.length} </span> </p>
+                <p className='text-[#b6bee5]'>Method: <span className='text-[#fff]'> {order.paymentMethod} </span> </p>
+                <p className='text-[#b6bee5]'>Payment: <span className='text-[#fff]'> { order.payment ? "Done" : "Pending"} </span> </p>
+                <p className='text-[#b6bee5]'>Date: <span className='text-[#fff]'> {new Date(order.date).toLocaleDateString() } </span> </p>
               </div>
 
               <p> {currency} {order.amount} </p>
+
+              <div>
+                <select className='bg-[#0d1224] border border-gray-500 p-2'>
+                  <option value="">- Select -</option>
+                  <option value="Payment Successful">Payment Successful</option>
+                  <option value="Transaction Failed">Transaction Failed</option>
+                </select>
+              </div>
+
             </div>
           ))
         }
