91580940ec7957a81c53803f4367e4607088a903
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-10-18 08:50:09 +0530

    Making button stylish

---
+++ Frontend/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Button.css";
+
+function Button() {
+  return (
+    <button
+        className="bracket-button relative border-[0.4px] border-white/15 bg-white/10 hover:bg-white/5 text-white text-[18px] font-bold px-9 py-4 cursor-pointer transition-all duration-150 ease-in-out">
+        BUTTON
+    </button>
+  )
+}
+
+export default Button;
