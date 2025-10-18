28c97aa6e699460f016a543feca6e247a77c6331
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-10-18 10:12:02 +0530

    Improvement 67

--- Frontend/src/components/Button.jsx
+++ Frontend/src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import "./Button.css";
 
-function Button() {
+function Button({ text }) {
   return (
     <button
-        className="bracket-button relative border-[0.4px] border-white/15 bg-white/10 hover:bg-white/5 text-white text-[18px] font-bold px-9 py-4 cursor-pointer transition-all duration-150 ease-in-out">
-        BUTTON
+        className="bracket-button relative border-[0.4px] border-white/15 bg-white/10 hover:bg-white/5 text-white text-[18px] font-bold px-3 sm:px-6 md:px-10 py-2 sm:py-3 text-xs sm:text-sm cursor-pointer transition-all duration-150 ease-in-out">
+        {text}
     </button>
   )
 }
--- Frontend/src/components/NewsletterBox.jsx
+++ Frontend/src/components/NewsletterBox.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './Button';
 
 function NewsletterBox() {
 
@@ -14,7 +15,8 @@ function NewsletterBox() {
 
         <form onSubmit={onSubmitHandler} action="" className='flex items-center mx-auto mt-4 border border-[#b6bee5]/50'>
             <input type="email" name="" id="" placeholder='Enter your email' className='outline-none w-52 sm:w-72 xl:w-96 px-3 py-1.5 sm:py-2.5 flex-1 text-sm sm:text-base' required />
-            <button type='submit' className='bg-[#181e33] hover:bg-[#181e33]/70 active:bg-[#181e33]/40 text-[#aeb8e7] font-semibold transition-all px-3 sm:px-6 md:px-10 py-2 sm:py-3 text-xs sm:text-sm'>SUBSCRIBE</button>
+            {/* <button type='submit' className='bg-[#181e33] hover:bg-[#181e33]/70 active:bg-[#181e33]/40 text-[#aeb8e7] font-semibold transition-all px-3 sm:px-6 md:px-10 py-2 sm:py-3 text-xs sm:text-sm'>SUBSCRIBE</button> */}
+            <Button text={"SUBSCRIBE"} />
         </form>
     </div>
   )
